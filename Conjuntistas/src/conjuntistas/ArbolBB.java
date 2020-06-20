67268046312262b2e541deedd3e8c0f461af1a18
Erick <Erick_carus@hotmail.com>
2020-06-20 10:36:32 -0300

    20/06/20

--- Conjuntistas/src/conjuntistas/ArbolBB.java
+++ Conjuntistas/src/conjuntistas/ArbolBB.java
@@ -16,7 +16,7 @@ public class ArbolBB {
     public void ArbolABB() {
         this.raiz = null;
     }
-
+//Metodo que dado un elemento, intenta insertarlo en el arbol, si ese elemento ya esta en el abrol retorna false.
     public boolean insertar(Comparable elemNuevo) {
         boolean exito;
         if (this.raiz == null) {
@@ -27,40 +27,40 @@ public class ArbolBB {
         }
         return exito;
     }
-
+    
     private boolean insertarAux(NodoABB n, Comparable elemNuevo) {
         boolean resultado = false;
-        //compara elemNuevo con el elemento del nodo actual, si es igual entonces ya esta en el arbol y retorna false
+        //compara elemNuevo con el elemento del nodo actual, si es igual entonces ya esta en el arbol y retorna false.
         if (n.getElemento().equals(elemNuevo)) {
             resultado = false;
         } else {
             if (n.getElemento().compareTo(elemNuevo) > 0) {
-                //si es menor y tiene hijo izquierdo null, lo setea como hijo izquierdo
+                //si es menor y tiene hijo izquierdo null, lo setea como hijo izquierdo.
                 if (n.getIzquierdo() == null) {
                     n.setIzquierdo(new NodoABB(elemNuevo, null, null));
                     resultado = true;
                 } else {
-                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo
+                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo.
                     resultado = insertarAux(n.getIzquierdo(), elemNuevo);
                 }
             } else {
-                //si es mayor y tiene hijo derecho null, lo setea como hijo derecho
+                //si es mayor y tiene hijo derecho null, lo setea como hijo derecho.
                 if (n.getDerecho() == null) {
                     n.setDerecho(new NodoABB(elemNuevo, null, null));
                     resultado = true;
                 } else {
-                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho
+                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho.
                     resultado = insertarAux(n.getDerecho(), elemNuevo);
                 }
             }
         }
         return resultado;
     }
-
+    //Metodo que dado un elemento, verifica que dicho elemento se encuentra dentro del arbol, si lo esta retorna true.
     public boolean pertenece(Comparable elemBusc) {
-        boolean pertenece=false;
-        
-        if (this.raiz != null && elemBusc!=null) {
+        boolean pertenece = false;
+
+        if (this.raiz != null && elemBusc != null) {
             pertenece = perteneceAux(this.raiz, elemBusc);
         }
         return pertenece;
@@ -68,39 +68,34 @@ public class ArbolBB {
 
     private boolean perteneceAux(NodoABB n, Comparable elemBusc) {
         boolean resultado = false;
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
+        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true. 
         if (n.getElemento().equals(elemBusc)) {
             resultado = true;
         } else {
             if (n.getElemento().compareTo(elemBusc) > 0) {
-                //si es menor y tiene hijo izquierdo null, retorna false ya quese llego a una hoja y no se encontro
+                //si es menor y tiene hijo izquierdo null, retorna false ya quese llego a una hoja y no se encontro.
                 if (n.getIzquierdo() == null) {
                     resultado = false;
                 } else {
-                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo
+                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo.
                     resultado = perteneceAux(n.getIzquierdo(), elemBusc);
                 }
             } else {
-                //si es mayor y tiene hijo derecho null, retorna false ya quese llego a una hoja y no se encontro
+                //si es mayor y tiene hijo derecho null, retorna false ya quese llego a una hoja y no se encontro.
                 if (n.getDerecho() == null) {
                     resultado = false;
                 } else {
-                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho
+                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho.
                     resultado = perteneceAux(n.getDerecho(), elemBusc);
                 }
             }
         }
         return resultado;
     }
-
+    //Metodo que dado un elemento, verifica que dicho elemento se encuentra dentro del arbol, si lo esta lo elimina.
     public boolean eliminar(Comparable elemBusc) {
-        boolean pertenece;
-        if (this.raiz == null) {
-            pertenece = false;
-        } else {
-            if (this.raiz.getElemento().equals(elemBusc)) {
-
-            }
+        boolean pertenece = false;
+        if (this.raiz != null) {
             pertenece = eliminarAux(null, this.raiz, elemBusc, 1);
         }
         return pertenece;
@@ -108,25 +103,25 @@ public class ArbolBB {
 
     private boolean eliminarAux(NodoABB padre, NodoABB hijo, Comparable elemBusc, int pos) {
         boolean resultado = false;
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
+        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true. 
         if (hijo.getElemento().equals(elemBusc)) {
             eliminarNodo(padre, hijo, pos);
             resultado = true;
         } else {
             if (hijo.getElemento().compareTo(elemBusc) > 0) {
-                //si es menor y tiene hijo izquierdo null, retorna false ya quese llego a una hoja y no se encontro
+                //si es menor y tiene hijo izquierdo null, retorna false ya quese llego a una hoja y no se encontro.
                 if (hijo.getIzquierdo() == null) {
                     resultado = false;
                 } else {
-                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo
+                    //si el hijo izquierdo es diferente de null, llama al metodo con el hijo izquierdo.
                     resultado = eliminarAux(hijo, hijo.getIzquierdo(), elemBusc, 0);
                 }
             } else {
-                //si es mayor y tiene hijo derecho null, retorna false ya quese llego a una hoja y no se encontro
+                //si es mayor y tiene hijo derecho null, retorna false ya quese llego a una hoja y no se encontro.
                 if (hijo.getDerecho() == null) {
                     resultado = false;
                 } else {
-                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho
+                    //si el hijo derecho es diferente de null, llama al metodo con el hijo derecho.
                     resultado = eliminarAux(hijo, hijo.getDerecho(), elemBusc, 1);
                 }
             }
@@ -135,20 +130,21 @@ public class ArbolBB {
     }
 
     private void eliminarNodo(NodoABB padre, NodoABB hijo, int pos) {
-        //si hijo no tiene hijos lo setea directamente en null
+        //si hijo no tiene hijos lo envia al caso 1.
         if (hijo.getIzquierdo() == null && hijo.getDerecho() == null) {
-            eliminarSinHijo(padre,pos);
+            eliminarSinHijo(padre, pos);
         } else {
-            //si solo tiene un hijo lo envia al caso 2
+            //si solo tiene un hijo lo envia al caso 2.
             if (hijo.getIzquierdo() == null || hijo.getDerecho() == null) {
                 eliminarConUnHijo(padre, hijo, pos);
             } else {
-                //si no, significa que tiene 2 hijos y lo envia al caso 3
+                //si no, significa que tiene 2 hijos y lo envia al caso 3.
                 eliminarConDosHijos(hijo);
             }
         }
 
     }
+// Caso 1: eliminar un nodo sin hijos.
 
     private void eliminarSinHijo(NodoABB padre, int pos) {
         if (padre == null) {
@@ -161,6 +157,7 @@ public class ArbolBB {
             }
         }
     }
+//Caso 2: eliminar un nodo que tiene 1 hijo.
 
     private void eliminarConUnHijo(NodoABB padre, NodoABB hijo, int pos) {
         NodoABB reemplazo;
@@ -179,8 +176,10 @@ public class ArbolBB {
             }
         }
     }
+    // Caso 3: eliminar un nodo que tiene 2 hijos.
 
     private void eliminarConDosHijos(NodoABB hijo) {
+        //Si el HD del nodo a eliminar no tiene HI, significa que es el menor nodo en la rama derecha.
         if (hijo.getDerecho().getIzquierdo() == null) {
             hijo.setElemento(hijo.getDerecho().getElemento());
             hijo.setDerecho(hijo.getDerecho().getDerecho());
@@ -191,19 +190,22 @@ public class ArbolBB {
 
     }
 
+    //Busca el menor elemento de un subArbol y retorna su elemento.
     private Comparable encontrarMinAux(NodoABB padre, NodoABB hijo) {
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true
         Comparable resp = null;
+        //Verifica si tiene hijo izquierdo, si no tiene.
+        //significa que es el menor elemento del SubArbol y retorna su elemento, y luego lo elimina.
         if (hijo.getIzquierdo() == null) {
             resp = hijo.getElemento();
             padre.setIzquierdo(hijo.getDerecho());
-
         } else {
+            //Si tiene hijo izquierdo entonces llama al Metodo recursivo.
             resp = encontrarMinAux(hijo, hijo.getIzquierdo());
         }
         return resp;
     }
 
+    //Metodo que elimina al menor elemento del Arbol.
     public boolean eliminarMin() {
         boolean resultado = false;
         if (this.raiz != null) {
@@ -217,8 +219,7 @@ public class ArbolBB {
         return resultado;
     }
 
-    private void eliminarMinAux(NodoABB padre, NodoABB hijo) {
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
+    private void eliminarMinAux(NodoABB padre, NodoABB hijo) { 
         if (hijo.getIzquierdo() == null) {
             padre.setIzquierdo(hijo.getDerecho());
         } else {
@@ -226,52 +227,7 @@ public class ArbolBB {
         }
     }
 
-    public Comparable minimoElem() {
-        Comparable resultado = null;
-        if (this.raiz != null) {
-            if (this.raiz.getIzquierdo() == null) {
-                resultado = this.raiz.getElemento();
-            } else {
-                resultado = minimoElemAux(this.raiz, this.raiz.getIzquierdo());
-            }
-        }
-        return resultado;
-    }
-
-    private Comparable minimoElemAux(NodoABB padre, NodoABB hijo) {
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
-        Comparable resultado;
-        if (hijo.getIzquierdo() == null) {
-            resultado = hijo.getElemento();
-        } else {
-            resultado = minimoElemAux(hijo, hijo.getIzquierdo());
-        }
-        return resultado;
-    }
-
-    public Comparable maximoElem() {
-        Comparable resultado = null;
-        if (this.raiz != null) {
-            if (this.raiz.getDerecho() == null) {
-                resultado = this.raiz.getElemento();
-            } else {
-                resultado = maximoElemAux(this.raiz, this.raiz.getDerecho());
-            }
-        }
-        return resultado;
-    }
-
-    private Comparable maximoElemAux(NodoABB padre, NodoABB hijo) {
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
-        Comparable resultado;
-        if (hijo.getDerecho() == null) {
-            resultado = hijo.getElemento();
-        } else {
-            resultado = maximoElemAux(hijo, hijo.getDerecho());
-        }
-        return resultado;
-    }
-
+    //Metodo que maximo al menor elemento del Arbol.
     public boolean eliminarMax() {
         boolean resultado = false;
         if (this.raiz != null) {
@@ -286,13 +242,59 @@ public class ArbolBB {
     }
 
     private void eliminarMaxAux(NodoABB padre, NodoABB hijo) {
-        //compara elemNuevo con el elemento del nodo actual,si es igual retorna true 
         if (hijo.getDerecho() == null) {
             padre.setDerecho(hijo.getIzquierdo());
         } else {
             eliminarMaxAux(hijo, hijo.getDerecho());
         }
     }
+    //Metodo que devuelve el minimo elemento de un arbol.
+
+    public Comparable minimoElem() {
+        Comparable resultado = null;
+        if (this.raiz != null) {
+            if (this.raiz.getIzquierdo() == null) {
+                resultado = this.raiz.getElemento();
+            } else {
+                resultado = minimoElemAux(this.raiz.getIzquierdo());
+            }
+        }
+        return resultado;
+    }
+
+    private Comparable minimoElemAux(NodoABB hijo) { 
+        Comparable resultado;
+        if (hijo.getIzquierdo() == null) {
+            resultado = hijo.getElemento();
+        } else {
+            resultado = minimoElemAux(hijo.getIzquierdo());
+        }
+        return resultado;
+    }
+//Metodo que devuelve el maximo elemento de un arbol.
+
+    public Comparable maximoElem() {
+        Comparable resultado = null;
+        if (this.raiz != null) {
+            if (this.raiz.getDerecho() == null) {
+                resultado = this.raiz.getElemento();
+            } else {
+                resultado = maximoElemAux(this.raiz.getDerecho());
+            }
+        }
+        return resultado;
+    }
+
+    private Comparable maximoElemAux(NodoABB hijo) { 
+        Comparable resultado;
+        if (hijo.getDerecho() == null) {
+            resultado = hijo.getElemento();
+        } else {
+            resultado = maximoElemAux(hijo.getDerecho());
+        }
+        return resultado;
+    }
+//Metodo que verifica si un arbol es vacio, si lo es retorna true.
 
     public boolean esVacio() {
         boolean vacio = true;
@@ -301,6 +303,7 @@ public class ArbolBB {
         }
         return vacio;
     }
+//Metodo que calcula la altura de un Arbol.
 
     public int altura() {
         int altura;
@@ -327,6 +330,7 @@ public class ArbolBB {
         }
         return altMayor;
     }
+//Metodo que dado un elemento retorna el nivel en el que se encuentra. 
 
     public int nivel(Object elemento) {
         int retorno = -1;
@@ -363,11 +367,13 @@ public class ArbolBB {
         }
         return retorno;
     }
+//Metodo que vaica el Arbol.
 
     public void vaciar() {
         this.raiz = null;
 
     }
+//Metodo que retorna un arbol clon con todos los elementos del arbol original.
 
     public ArbolBB clone() {
         ArbolBB clon = new ArbolBB();
@@ -393,25 +399,27 @@ public class ArbolBB {
 
         }
     }
+//Lista todos los elementos de mayor a menor.
 
     public Lista listar() {
-        Lista inorden = new Lista();
-        inordenAux(this.raiz, inorden);
-        return inorden;
+        Lista list = new Lista();
+        ListarAux(this.raiz, list);
+        return list;
     }
 
-    private void inordenAux(NodoABB raiz, Lista inorden) {
+    private void ListarAux(NodoABB raiz, Lista list) {
         if (raiz != null) {
-            inordenAux(raiz.getIzquierdo(), inorden);
-            inorden.insertar(raiz.getElemento(), inorden.longitud() + 1);
-            inordenAux(raiz.getDerecho(), inorden);
+            ListarAux(raiz.getIzquierdo(), list);
+            list.insertar(raiz.getElemento(), list.longitud() + 1);
+            ListarAux(raiz.getDerecho(), list);
         }
     }
+//Lista todos los elementos que se encuentran en un rango determinado.
 
     public Lista listarRango(Comparable min, Comparable max) {
         Lista list = new Lista();
-        if (min != null && max!= null) {
-        listarRangoAux(this.raiz, list, min, max);
+        if (min != null && max != null) {
+            listarRangoAux(this.raiz, list, min, max);
         }
         return list;
     }
@@ -432,6 +440,7 @@ public class ArbolBB {
     }
 
     @Override
+    //Metodo que devuelve un String que muestra a todos los nodos y quienes son sus hijos.
     public String toString() {
         return (this.raiz != null)
                 ? toStringAux(this.raiz, "") : "Arbol Vacio";
